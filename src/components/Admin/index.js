e331e502f317b07d5ab8bc65becb310af5a8b8ab
AustinSpinazze <spinazzeaustin@yahoo.com>
2020-02-19 09:10:16 -0600

    Added protected routes

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
-const App = () => (
+import withAuthorization from '../Session/withAuthorization';
+import * as ROLES from '../../constants/roles';
+
+const AdminPage = () => (
   <div>
-    <h1>App</h1>
+    <h1>Admin</h1>
+    <p>
+      Restricted area! Only users with the admin role are authorized.
+    </p>
   </div>
 );
-export default App;
+
+const condition = authUser => authUser && !!authUser.roles[ROLES.ADMIN];
+
+export default withAuthorization(condition)(AdminPage);
--- src/components/Session/withAuthentication.js
+++ src/components/Session/withAuthentication.js
@@ -1,41 +1,42 @@
 import React from 'react';
+
 import AuthUserContext from './context';
 import { withFirebase } from '../Firebase';
 
 const withAuthentication = Component => {
-    class WithAuthentication extends React.Component {
-        constructor(props) {
-            super(props);
-        
-            this.state = { authUser: null };
-        }
+  class WithAuthentication extends React.Component {
+    constructor(props) {
+      super(props);
 
-        componentDidMount() {
-            this.listener = this.props.firebase.auth.onAuthStateChanged(
-              authUser => {
-                authUser
-                  ? this.setState({ authUser })
-                  : this.setState({ authUser: null });
-              },
-            );
-        }
-
-        componentWillUnmount() {
-            // We also want to avoid memory leaks that lead to performance issues, so we'll remove the listener if the component unmounts.
-            this.listener();
-        }
-
-
-        render() {
-            return (
-                <AuthUserContext.Provider value={this.state.authUser}>
-                    <Component {...this.props} />
-                </AuthUserContext.Provider>
-            );
-        }
+      this.state = {
+        authUser: null,
+      };
     }
 
-    return withFirebase(WithAuthentication);
+    componentDidMount() {
+      this.listener = this.props.firebase.auth.onAuthStateChanged(
+        authUser => {
+          authUser
+            ? this.setState({ authUser })
+            : this.setState({ authUser: null });
+        },
+      );
+    }
+
+    componentWillUnmount() {
+      this.listener();
+    }
+
+    render() {
+      return (
+        <AuthUserContext.Provider value={this.state.authUser}>
+          <Component {...this.props} />
+        </AuthUserContext.Provider>
+      );
+    }
+  }
+
+  return withFirebase(WithAuthentication);
 };
 
 export default withAuthentication;
